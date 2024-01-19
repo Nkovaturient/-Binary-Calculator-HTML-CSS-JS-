660de098323c5ef2af76db242ca7a06606ca7bb8
Neha_Kumari711 <nk8981398@gmail.com>
2024-01-19 18:12:05 +0530

    set styling and js code

---
+++ app.js
@@ -0,0 +1,30 @@
+const display=document.querySelector("#display");
+const btns=document.querySelectorAll("button");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+    
+        if(btn.id === "="){
+            display.value = eval(display.value);
+        } else if (btn.id === "ac"){
+            display.value = "";
+        } else if (btn.id === "de"){
+            display.value = display.value.slice(0, -1);
+        } else if (btn.id === "lg"){
+            display.value = Math.log10(display.value);
+        } else if (btn.id === "sqroot"){
+            display.value = Math.sqrt(display.value);
+        } else if (btn.id === "sin"){
+            display.value = Math.sin(display.value);
+        } else if (btn.id === "cos"){
+            display.value = Math.cos(display.value);
+        } else if (btn.id === "tan"){
+            display.value = Math.tan(display.value);
+        } 
+        else{
+            display.value += btn.id;
+        }
+    });
+    
+});
+ 
